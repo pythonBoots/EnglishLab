358b0b5076a55184132382ae5f91b12148e54a3a
artemiy.krot <artemiy.krot@gmail.com>
2020-08-21 12:43:06 +0300

    Работа с шаблоном index.html

--- EnglishLab/EnglishLab/settings.py
+++ EnglishLab/EnglishLab/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Add your apps here to enable them
+    'test_pass',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- EnglishLab/test_pass/views.py
+++ EnglishLab/test_pass/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import datetime
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "test_pass/index.html",
+        {
+            'title': "Hello Django!",
+            'message': "Hello Django!", 
+            'content': " on "  + now.strftime("%A,%d %B, %Y at %X")
+        }
+    )
+
